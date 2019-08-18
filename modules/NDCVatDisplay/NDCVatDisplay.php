8afd7aace810b7e38a2a4f1ac30735213ed39eb7
rp <tendedzero@gmail.com>
2019-08-18 19:27:36 +0100

    added in admin changes, rewrote class to install rrp new val checkover

--- modules/NDCVatDisplay/NDCVatDisplay.php
+++ modules/NDCVatDisplay/NDCVatDisplay.php
@@ -5,103 +5,99 @@
 * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 */
 
-use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
 use PrestaShop\PrestaShop\Adapter\ObjectPresenter;
 
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class NDCVatDisplay extends Module implements WidgetInterface
+class NDCVatDisplay extends Module
 {
      public function __construct()
     {
         $this->name = 'NDCVatDisplay';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.5';
+        $this->version = '1.0.6';
         $this->author = 'RPaterson';
         $this->need_instance = 0;
-
         $this->bootstrap = true;
         parent::__construct();
-
-        $this->displayName = $this->l('NDC Vat & RRP Display');
-        $this->description = $this->l('Displays VAT & RRP.');
+        $this->displayName = $this->l('NDC Vat  RRP Display');
+        $this->description = $this->l('Displays VAT  RRP.');
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
-
-        $this->templateFile = 'module:NDCVatDisplay/NDCVatDisplay.tpl';
     }
     
    public function install()
 {
-    if (Shop::isFeatureActive())
-        Shop::setContext(Shop::CONTEXT_ALL);
+    if (!parent::install() || !$this->_installSql()
+        //Pour les hooks suivants regarder le fichier src\PrestaShopBundle\Resources\views\Admin\Product\form.html.twig
+        || ! $this->registerHook('displayAdminProductsExtra')
+        || ! $this->registerHook('displayNav2')
+        || ! $this->registerHook('displayProductPriceBlock')
+    ) {
+        return false;
+    }
 
-    return parent::install() &&
-        $this->registerHook('displayNav2');
+    return true;
 }
 
 public function uninstall()
 {
-    if (!parent::uninstall())
-        return false;
-    return true;
+    return parent::uninstall() && $this->_unInstallSql();
 }
-    
-    public function displayForm()
-{
-    // < init fields for form array >
- 
-    // < load helperForm >
-    $helper = new HelperForm();
-
-    // < module, token and currentIndex >
-    $helper->module = $this;
-    $helper->name_controller = $this->name;
-    $helper->token = Tools::getAdminTokenLite('AdminModules');
-    $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
-
-    // < title and toolbar >
-    $helper->title = $this->displayName;
-    $helper->show_toolbar = true;        // false -> remove toolbar
-    $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
-    
-
- 
-}
+    /**
+     * Modifications sql du module
+     * @return boolean
+     */
+    protected function _installSql() {
+        $sqlInstall = "ALTER TABLE " . _DB_PREFIX_ . "product "
+            . "ADD rrp_price_ex decimal(20,6) NULL";
+        $returnSql = Db::getInstance()->execute($sqlInstall);
+        return $returnSql;
+    }
+    /**
+     * Uninstall RRP Fields
+     * @return boolean
+     */
+    protected function _unInstallSql() {
+        $sqlInstall = "ALTER TABLE " . _DB_PREFIX_ . "product "
+            . "DROP rrp_price_ex";
+        $returnSql = Db::getInstance()->execute($sqlInstall);
+        return $returnSql;
+    }
 
 public function getContent()
 {
     $output = null;
     return $output.$this->displayForm();
 }
-
-
-
-public function getWidgetVariables($hookName, array $configuration)
-{
-    $vatmode = null;
-    $link = $this->context->link;
-    $cookieKey = 'VATMODE';
-    if (!isset($_COOKIE['VATMODE'])) {
-        $this->context->cookie->__set($cookieKey, "true");
-        $vatmode = 'true';
-    }
-    else
-    {
-        $vatmode = $_COOKIE['VATMODE'];
-    }
-    return array(
-        'vlink' => $link,
-        'vatmode' => $vatmode,
-    );
-}
-public function renderWidget($hookName, array $configuration)
+    public function hookDisplayNav2($params)
     {
         if (Configuration::isCatalogMode())  {
             return false;
         }
-        $this->smarty->assign($this->getWidgetVariables($hookName, $configuration));
-        return $this->fetch($this->templateFile);
+        return $this->display(__FILE__, 'views/templates/hook/front/VAT_DisplayMod.tpl');
     }
+
+    public function hookDisplayProductPriceBlock($params)
+    {
+        if (Configuration::isCatalogMode())  {
+            return false;
+        }
+    }
+    /**
+     * Params for hook
+     * @param type $params
+     * @return type
+     */
+    public function hookDisplayAdminProductsExtra($params) {
+        $product = new Product($params['id_product']);
+        $this->context->smarty->assign(array(
+                'rrp_price_ex' => $product->rrp_price_ex,
+                'default_language' => $this->context->employee->id_lang,
+            )
+        );
+        return $this->display(__FILE__, 'views/templates/hook/RRP_Display.tpl');
+    }
+
 }
